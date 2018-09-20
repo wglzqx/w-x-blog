464fe5dcf471a8c6fdae1dc9bf335c9a92fa0926
XuXin <xuxin@dingyantech.com>
2018-09-20 16:58:47 +0800

    Initial commit

---
+++ src/main/java/com/wxblog/WXBlogApplication.java
@@ -0,0 +1,12 @@
+package com.wxblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WXBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WXBlogApplication.class, args);
+    }
+}
